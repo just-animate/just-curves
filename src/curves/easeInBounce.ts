88c6173d6ae0666eb5478517a011e1046bf032cc
Christopher Wallis <christopher.j.wallis@gmail.com>
2019-09-02 22:09:48 -0600

    fixed circular dependency and updated packages

--- src/curves/easeInBounce.ts
+++ src/curves/easeInBounce.ts
@@ -1,3 +1,3 @@
-import { easeOutBounce } from './index';
+import { easeOutBounce } from './easeOutBounce';
 
 export const easeInBounce = (x: number): number => 1 - easeOutBounce(1 - x);
--- src/curves/easeInOutElastic.ts
+++ src/curves/easeInOutElastic.ts
@@ -1,7 +1,9 @@
 import { pow, sin, tau } from '../internal';
 
 export const easeInOutElastic = (n: number) => {
-  if (!n || n === 1) return n;
+  if (!n || n === 1) {
+    return n;
+  }
   n *= 2;
   if (n < 1) {
       return -0.5 * (pow(2, 10 * (n - 1)) * sin((n - 1.1) * tau / .4));
